be21f6bdf03338d94f3df486e83012cdbd2b82ef
Hristo Tanev <hristotanev7@gmail.com>
2017-07-06 15:03:03 +0100

    Created a chart for the stock data

--- src/js/components/StockChart.js
+++ src/js/components/StockChart.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 export default class StockChart extends React.Component {
+  componentWillMount() {
+      anychart.onDocumentReady(() => {
+        let table = anychart.data.table()
+        let mapping = table.mapAs()
+        let chart = anychart.stock()
+        chart.plot(0).ohlc(mapping).name('Test')
+        chart.title('Test title')
+        chart.container('chart')
+        chart.draw()
+      })
+  }
+
   render() {
     return (
       <div id="chart" class="stock-chart"></div>
